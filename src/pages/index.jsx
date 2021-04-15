c44cee4d3dd1c2aef93554d89e199f64779957da
Saulo <duart.herrera@gmail.com>
2021-04-15 09:16:53 -0300

    tailwind ready to roll!

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import Card from '@material-ui/core/Card'
+// import Card from '@material-ui/core/Card'
+// import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
-      <Card>
-        <h1>Uniconn</h1>
-      </Card>
+      <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+       {/* Next img don't work properly, see this issue later */}
+        <img
+          className="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0"
+          alt="Woman's Face"
+          src="https://images.unsplash.com/photo-1509967419530-da38b4704bc6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d29tYW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
+        ></img>
+        <div className="text-center space-y-2 sm:text-left">
+          <div className="space-y-0.5">
+            <p className="text-lg text-black font-semibold">Erin Lindford</p>
+            <p className="text-gray-500 font-medium">Product Engineer</p>
+          </div>
+          <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+            Message
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
