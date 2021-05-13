b706f64eaa2d979eeef35a2da0eec72b21b7b08d
felipe-carvalho12 <felipebarrettocarvalho@gmail.com>
2021-05-13 14:57:03 -0300

    landing page...

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react'
+import Link from 'next/link'
 import Head from 'next/head'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import Router from 'next/router'
@@ -40,36 +41,46 @@ export default function Index() {
         <title>Uniconn</title>
       </Head>
       <div className="w-screen h-screen pt-20">
-        <div className="fixed top-0 w-full h-20 flex items-center px-4 bg-dark b-bottom-light">
+        <div className="fixed top-0 z-10 w-full h-20 flex items-center px-4 bg-dark b-bottom-light">
           <div>
             <h1>Uniconn</h1>
           </div>
           <div className="ml-auto flex h-2/3">
-            <button className="btn-secondary m-2">Entrar</button>
-            <button className="btn-primary m-2">Criar conta</button>
+            <Link href="/login">
+              <button className="btn-secondary m-2">Entrar</button>
+            </Link>
+            <Link href="/signup">
+              <button className="btn-primary m-2">Criar conta</button>
+            </Link>
           </div>
         </div>
         <div className="bg-linear flex px-32 pt-32 pb-20">
           <div className="w-1/2">
             <h1>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-              iste blanditiis quae cum minima
+              Lorem ipsum dolor sit amet{' '}
+              <span style={{ color: 'var(--secondary-color)' }}>
+                consectetur adipisicing elit
+              </span>
+              . Minus iste blanditiis quae cum minima
             </h1>
-            <p className="my-4">
+            <p className="mt-4 mb-8">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
               iste blanditiis quae cum minima, quam eos eius est repellendus
               dolores voluptatum fuga asperiores, nihil, amet in reprehenderit
               laboriosam architecto nesciunt.
             </p>
-            <button className="btn-primary text-xl">Criar conta</button>
+            <Link href="/signup">
+              <button className="btn-primary text-xl">Criar conta</button>
+            </Link>
           </div>
         </div>
         <div className="bg-light flex px-32 pt-32 pb-20">
           <div className="w-1/2">
-            <h1>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-              iste blanditiis quae cum minima
-            </h1>
+            <h2>
+              <span style={{ color: 'var(--secondary-color)' }}>1.</span> Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Minus iste
+              blanditiis quae cum minima
+            </h2>
             <p className="my-4">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
               iste blanditiis quae cum minima, quam eos eius est repellendus
@@ -88,23 +99,65 @@ export default function Index() {
             >
               <path
                 d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-                className="shape-fill"
+                fill="var(--background-light)"
               ></path>
             </svg>
           </div>
         </div>
         <div className="bg-dark flex px-32 pt-32 pb-20">
           <div className="w-1/2">
-            <h1>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-              iste blanditiis quae cum minima
-            </h1>
+            <h2>
+              <span style={{ color: 'var(--secondary-color)' }}>2.</span> Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Minus iste
+              blanditiis quae cum minima
+            </h2>
             <p className="my-4">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
               iste blanditiis quae cum minima, quam eos eius est repellendus
               dolores voluptatum fuga asperiores, nihil, amet in reprehenderit
               laboriosam architecto nesciunt.
             </p>
+          </div>
+        </div>
+        <div className="w-full relative">
+          <div className="wave">
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
+            >
+              <path
+                d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                fill="var(--background-dark)"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <div className="bg-light flex px-32 pt-32 pb-20">
+          <div className="w-1/2">
+            <h2>
+              <span style={{ color: 'var(--secondary-color)' }}>3.</span> Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Minus iste
+              blanditiis quae cum minima
+            </h2>
+            <p className="my-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+              iste blanditiis quae cum minima, quam eos eius est repellendus
+              dolores voluptatum fuga asperiores, nihil, amet in reprehenderit
+              laboriosam architecto nesciunt.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center w-full pt-10 pb-20">
+          <h2>Uniconn</h2>
+          <div className="flex">
+            <Link href="/login">
+              <button className="btn-secondary m-2 text-xl">Entrar</button>
+            </Link>
+            <Link href="/signup">
+              <button className="btn-primary m-2 text-xl">Criar conta</button>
+            </Link>
           </div>
         </div>
       </div>
