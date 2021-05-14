2e43d4ff194cd3d0d0d87cad10799c47e9af60c5
felipe-carvalho12 <felipebarrettocarvalho@gmail.com>
2021-05-14 15:54:15 -0300

    project list item...

--- src/components/pages/home/ProfileMetrics.jsx
+++ src/components/pages/home/ProfileMetrics.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { MyProfileContext } from '../../../contexts/MyProfile'
 
 function ProfileMetrics() {
+  const { myProfile } = useContext(MyProfileContext)
+
+  if (!myProfile) {
+    return <CircularProgress />
+  }
+
   return (
     <div
-      className="bg-transparent h-full rounded-md shadow-lg"
+      className="bg-transparent h-full rounded-md shadow-lg pt-4"
       style={{ width: 225 }}
-    ></div>
+    >
+      <div className="flex flex-col items-center">
+        <img
+          src={process.env.NEXT_PUBLIC_API_HOST + myProfile.photo}
+          className="profile-img-lg"
+        />
+        <h4 className="mt-2">
+          {myProfile.first_name} {myProfile.last_name}
+        </h4>
+      </div>
+    </div>
   )
 }
 
