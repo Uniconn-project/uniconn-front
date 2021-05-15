a4f7ff0c31c2721ad10172f49af17726ff51027e
felipe-carvalho12 <felipebarrettocarvalho@gmail.com>
2021-05-15 10:55:02 -0300

    [=

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -17,17 +17,14 @@ export default function Home() {
   return (
     <Page title="Home | Uniconn" page="home" loginRequired header>
       <div className="justify-center w-full h-full flex">
-        <div className="hidden lg:flex-grow lg:flex lg:justify-end lg:mr-10 lg:box-border">
+        <div className="hidden lg:w-1/3 lg:flex lg:justify-end lg:mr-10 lg:box-border">
           <div style={{ width: 225 }}>
             <div className="h-full fixed top-32">
               <ProfileMetrics />
             </div>
           </div>
         </div>
-        <div
-          className="flex justify-center lg:justify-start lg:box-border"
-          style={{ flexGrow: 2 }}
-        >
+        <div className="w-full flex justify-center p-2 lg:w-2/3 lg:justify-start lg:box-border">
           <div className="w-full" style={{ maxWidth: 600 }}>
             <ProjectsFilter
               projects={projects}
