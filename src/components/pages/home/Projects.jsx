5200c1fbdea93d38777490ccbac829fc06dd82c5
felipe-carvalho12 <felipebarrettocarvalho@gmail.com>
2021-05-17 23:44:20 -0300

    my profile page...

--- src/components/pages/home/Projects.jsx
+++ src/components/pages/home/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { CircularProgress } from '@material-ui/core'
-import ProjectListItem from './ProjectListItem'
+import ProjectListItem from '../../global/ProjectListItem'
 
 function Projects({ renderedProjects }) {
   if (!renderedProjects) {
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,10 +1,24 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import ProfileInfo from '../components/global/ProfileInfo'
 import Page from '../components/Page'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import { MyProfileContext } from '../contexts/MyProfile'
+import { fetcher } from '../hooks/useFetch'
+import ProjectListItem from '../components/global/ProjectListItem'
 
 export default function MyProfile() {
   const { myProfile } = useContext(MyProfileContext)
+  const [projects, setProjects] = useState(null)
+
+  useEffect(() => {
+    if (!myProfile) return
+    ;(async () => {
+      const data = await fetcher(
+        `profiles/get-profile-projects/${myProfile.user.username}`
+      )
+      await setProjects(data)
+    })()
+  }, [myProfile])
 
   return (
     <Page page="profile" loginRequired header>
@@ -18,7 +32,24 @@ export default function MyProfile() {
         </div>
         <div className="w-full flex justify-center p-2 pt-0 lg:p-0 lg:w-2/3 lg:justify-start lg:box-border">
           <div className="w-full" style={{ maxWidth: 600 }}>
-            <div className="sticky top-24 bg-light w-full h-14 rounded-md shadow-lg p-2 mb-4 flex justify-center items-center sm:top-32"></div>
+            <div className="sticky top-24 bg-light w-full h-14 rounded-md shadow-lg p-2 mb-4 flex justify-center items-center sm:top-32">
+              {projects !== null ? (
+                <span>Meus projetos ({projects.length})</span>
+              ) : (
+                <CircularProgress size={30} />
+              )}
+            </div>
+            <div className="w-full px-2">
+              {projects !== null ? (
+                <>
+                  {projects.map(project => (
+                    <ProjectListItem key={project.id} project={project} />
+                  ))}
+                </>
+              ) : (
+                <CircularProgress />
+              )}
+            </div>
           </div>
         </div>
       </div>
