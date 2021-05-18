eedcdcfb71fa80a5404c950dbc0ae4f68ca0a388
felipe-carvalho12 <felipebarrettocarvalho@gmail.com>
2021-05-17 21:51:49 -0300

    profile data

--- src/components/pages/home/ProfileInfo.jsx
+++ src/components/pages/home/ProfileInfo.jsx
@@ -2,6 +2,9 @@ import React, { useContext } from 'react'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import SchoolIcon from '@material-ui/icons/School'
 import AssignmentIcon from '@material-ui/icons/Assignment'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import Tooltip from '@material-ui/core/Tooltip'
+import EditIcon from '@material-ui/icons/Edit'
 import { MyProfileContext } from '../../../contexts/MyProfile'
 
 function ProfileMetrics() {
@@ -11,12 +14,13 @@ function ProfileMetrics() {
     return <CircularProgress />
   }
 
+  const major = myProfile.type === 'student' && myProfile.student.major.name
+
   return (
     <div
-      className="bg-transparent h-4/5 rounded-md shadow-lg pt-4"
+      className="relative bg-transparent h-4/5 rounded-md shadow-lg pt-4"
       style={{ width: 225 }}
     >
-      {console.log(myProfile)}
       <div className="b-bottom-light pb-6">
         <div className="w-full flex justify-center">
           <div className="relative">
@@ -35,12 +39,32 @@ function ProfileMetrics() {
           <h4 className="mt-2">
             {myProfile.first_name} {myProfile.last_name}
           </h4>
-          <p className="self-start color-secondary">
+          <p className="self-start break-all color-secondary">
             @{myProfile.user.username}
           </p>
         </div>
       </div>
-      <div></div>
+      <div className="w-full pl-4 pr-2 py-6 b-bottom-light">
+        <p className="break-all">{myProfile.bio}</p>
+      </div>
+      <div className="w-full pl-4 pr-1 pt-6 ">
+        <ul>
+          <li className="pb-2">
+            <SchoolIcon className="icon-sm" />{' '}
+            {major[0].toUpperCase() + major.slice(1)}
+          </li>
+          {myProfile.linkedIn !== '' && (
+            <li className="pb-2">
+              <LinkedInIcon className="icon-sm" /> {myProfile.linkedIn}
+            </li>
+          )}
+        </ul>
+      </div>
+      <div className="absolute bottom-2 right-2 p-2 rounded-3xl cursor-pointer bg-secondary bg-hover color-bg-light">
+        <Tooltip title="Editar perfil" placement="top" arrow>
+          <EditIcon className="icon-sm" />
+        </Tooltip>
+      </div>
     </div>
   )
 }
