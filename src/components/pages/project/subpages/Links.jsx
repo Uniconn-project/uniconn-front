0955617f864ba09a9785eafd4eb34f180ca52954
Saulo <duart.herrera@gmail.com>
2021-06-03 17:15:13 -0300

    links render added

--- src/components/pages/project/subpages/Links.jsx
+++ src/components/pages/project/subpages/Links.jsx
@@ -9,6 +9,18 @@ export default function Links({ project }) {
       </div>
     )
   }
-
-  return <div className="p-2">Links</div>
+  console.log(project)
+  return (
+    <div className="p-2">
+      {project.links.map(link => (
+        <a href={link.href} target="_blank" rel="noreferrer" key={link.id} className="no-underline">
+          <div
+            className="p-4 bg-transparent rounded-md shadow-lg mb-4 color-paragraph bg-hover"
+          >
+            <div className="no-underline">{link.name}</div>
+          </div>
+        </a>
+      ))}
+    </div>
+  )
 }
