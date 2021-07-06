9ac8bdb9fe0f0da2bafa0f20c3cc402c285833a2
Felipe Carvalho <felipebarrettocarvalho@gmail.com>
2021-07-05 21:25:33 -0300

    FIXED: mentor markets names not appearing

--- src/components/global/profile-info/components/MentorInfo.jsx
+++ src/components/global/profile-info/components/MentorInfo.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import AssignmentIcon from '@material-ui/icons/Assignment'
 import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import useFetch from '../../../../hooks/useFetch'
 
 export default function MentorInfo({ profile }) {
+  const { data: markets } = useFetch(
+    `profiles/get-mentor-markets/${profile.user.username}`
+  )
+
+  if (!markets) {
+    return <CircularProgress size={20} />
+  }
+
   return (
     <li className="pb-2">
       <div className="w-full flex items-center mb-1">
@@ -11,7 +21,7 @@ export default function MentorInfo({ profile }) {
         </span>
       </div>
       <ul className="max-h-24 overflow-y-auto">
-        {profile.mentor.markets.map(market => (
+        {markets.map(market => (
           <li key={market.id}>
             <ArrowRightAltIcon className="color-primary icon-sm" />{' '}
             {market.name}
