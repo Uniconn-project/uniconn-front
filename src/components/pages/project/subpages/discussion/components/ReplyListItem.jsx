a837d23499150f1a4791428111804066fe5e9969
Felipe Carvalho <felipebarrettocarvalho@gmail.com>
2021-07-09 15:44:23 -0300

    rendering timestamp at star notification

--- src/components/pages/project/subpages/discussion/components/ReplyListItem.jsx
+++ src/components/pages/project/subpages/discussion/components/ReplyListItem.jsx
@@ -4,6 +4,7 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import { mutate } from 'swr'
 import { MyProfileContext } from '../../../../../../contexts/MyProfile'
 import { AuthContext } from '../../../../../../contexts/Auth'
+import { renderTimestamp } from '../../../../../../utils/utils'
 
 export default function ReplyListItem({
   discussion,
@@ -46,7 +47,7 @@ export default function ReplyListItem({
 
   return (
     <div className="bg-transparent rounded-md shadow-lg mb-4">
-      <div className="flex justify-between">
+      <div className="flex justify-between items-start">
         <div className="flex flex-col b-bottom-light p-2 sm:flex-row">
           <div className="mr-2">
             <Link href={`/user/${reply.profile.user.username}`}>
@@ -65,11 +66,14 @@ export default function ReplyListItem({
             </p>
           </div>
         </div>
-        {myProfile.id === reply.profile.id && (
-          <div className="cursor-pointer p-2" onClick={handleDelete}>
-            <DeleteIcon className="icon-sm color-secondary-hover" />
-          </div>
-        )}
+        <div className="flex items-start p-2">
+          <div>{renderTimestamp(reply.created_at)}</div>
+          {myProfile.id === reply.profile.id && (
+            <div className="cursor-pointer ml-2" onClick={handleDelete}>
+              <DeleteIcon className="icon-sm color-secondary-hover" />
+            </div>
+          )}
+        </div>
       </div>
       <div className="p-2">
         <p>{reply.content}</p>
