b931eacadc34c5762472cf45bc049dcee2fca8ad
Felipe Carvalho <felipebarrettocarvalho@gmail.com>
2021-07-23 02:33:48 -0300

    started members project subpage

--- src/components/pages/project/ProjectHeader.jsx
+++ src/components/pages/project/ProjectHeader.jsx
@@ -3,6 +3,7 @@ import CircularProgress from '@material-ui/core/CircularProgress'
 import Tooltip from '@material-ui/core/Tooltip'
 import DescriptionIcon from '@material-ui/icons/Description'
 import ForumIcon from '@material-ui/icons/Forum'
+import GroupIcon from '@material-ui/icons/Group'
 import LinkIcon from '@material-ui/icons/Link'
 import BuildIcon from '@material-ui/icons/Build'
 import { MyProfileContext } from '../../../contexts/MyProfile'
@@ -21,11 +22,11 @@ export default function ProjectHeader({ project, page, setPage }) {
 
   return (
     <div className="sticky top-24 w-full mb-4 z-10 sm:top-32">
-      <div className="w-full bg-light h-14 rounded-md shadow-lg p-2 flex items-center">
-        <div className="w-full flex px-4">
+      <div className="w-full bg-light h-14 rounded-md shadow-lg py-2 flex items-center">
+        <div className="w-full flex">
           <Tooltip title="Descrição" placement="top" arrow>
             <div
-              className={`project-menu-item p-3 mr-2 nav-link cursor-pointer color-headline-hover flex justify-center items-center flex-grow ${
+              className={`project-menu-item p-3 nav-link cursor-pointer color-headline-hover flex justify-center items-center flex-grow ${
                 page === 'description' ? 'b-bottom-primary' : ''
               }`}
               onClick={() => setPage('description')}
@@ -35,7 +36,7 @@ export default function ProjectHeader({ project, page, setPage }) {
           </Tooltip>
           <Tooltip title="Discussões" placement="top" arrow>
             <div
-              className={`project-menu-item p-3 ml-2 nav-link cursor-pointer color-headline-hover flex justify-center items-center flex-grow ${
+              className={`project-menu-item p-3 nav-link cursor-pointer color-headline-hover flex justify-center items-center flex-grow ${
                 page === 'discussions' || page === 'discussion'
                   ? 'b-bottom-primary'
                   : ''
@@ -46,9 +47,20 @@ export default function ProjectHeader({ project, page, setPage }) {
               <span>({project.discussions_length})</span>
             </div>
           </Tooltip>
+          <Tooltip title="Equipe" placement="top" arrow>
+            <div
+              className={`project-menu-item p-3 nav-link cursor-pointer color-headline-hover flex justify-center items-center flex-grow ${
+                page === 'members' ? 'b-bottom-primary' : ''
+              }`}
+              onClick={() => setPage('members')}
+            >
+              <GroupIcon className="icon-sm" />{' '}
+              <span>({project.students.concat(project.mentors).length})</span>
+            </div>
+          </Tooltip>
           <Tooltip title="Links" placement="top" arrow>
             <div
-              className={`project-menu-item p-3 ml-2 nav-link cursor-pointer color-headline-hover flex justify-center items-center flex-grow ${
+              className={`project-menu-item p-3 nav-link cursor-pointer color-headline-hover flex justify-center items-center flex-grow ${
                 page === 'links' ? 'b-bottom-primary' : ''
               }`}
               onClick={() => setPage('links')}
@@ -60,7 +72,7 @@ export default function ProjectHeader({ project, page, setPage }) {
           {isProjectMember && (
             <Tooltip title="Ferramentas" placement="top" arrow>
               <div
-                className={`project-menu-item p-3 ml-2 nav-link cursor-pointer color-headline-hover flex justify-center items-center flex-grow ${
+                className={`project-menu-item p-3 nav-link cursor-pointer color-headline-hover flex justify-center items-center flex-grow ${
                   page === 'tools' ? 'b-bottom-primary' : ''
                 }`}
                 onClick={() => setPage('tools')}
--- src/components/pages/project/subpages/members/Members.jsx
+++ src/components/pages/project/subpages/members/Members.jsx
@@ -1,163 +1,20 @@
-import React, { useContext, useState } from 'react'
-import CircularProgress from '@material-ui/core/CircularProgress'
-import CloseIcon from '@material-ui/icons/Close'
-import Snackbar from '@material-ui/core/Snackbar'
-import Alert from '@material-ui/lab/Alert'
-import ProfileListItem from '../../../../global/ProfileListItem'
-import ProfileListItemWithIcon from '../../../../global/ProfileListItemWithIcon'
-import AddMembersModal from './components/AddMembersModal'
-import AskToJoinProjectModal from './components/AskToJoinProjectModal'
-import SettingsPopover from './components/SettingsPopover'
-import { MyProfileContext } from '../../../../../contexts/MyProfile'
-import { AuthContext } from '../../../../../contexts/Auth'
-import LeaveProject from './components/LeaveProject'
+import React from 'react'
+import MembersDesktop from './components/MembersDesktop'
+import MembersMobile from './components/MembersMobile'
 
-export default function Members({ type, project, refetchProject }) {
-  const { myProfile } = useContext(MyProfileContext)
-  const { getToken } = useContext(AuthContext)
+const tailwindConfig = require('../../../../../../tailwind.config')
 
-  const [errorMsg, setErrorMsg] = useState({
-    isOpen: false,
-    message: ''
-  })
-
-  const projectStudentsId = project.students.map(profile => profile.id)
-
-  const projectMembersId = projectStudentsId.concat(
-    project.mentors.map(profile => profile.id)
-  )
-
-  const handleCancelProjectInvitation = async (e, username) => {
-    e.stopPropagation()
-
-    if (window.confirm('Remover convite?')) {
-      fetch(
-        `${process.env.NEXT_PUBLIC_API_HOST}/api/projects/uninvite-${type}-from-project/${project.id}`,
-        {
-          method: 'PUT',
-          headers: {
-            Authorization: 'JWT ' + (await getToken()),
-            'Content-type': 'application/json'
-          },
-          body: JSON.stringify({
-            username: username
-          })
-        }
-      )
-        .then(response => response.json())
-        .then(data => {
-          if (data === 'success') {
-            refetchProject('uninvite-user')
-          } else {
-            setErrorMsg({
-              isOpen: true,
-              message: data
-            })
-          }
-        })
-    }
-  }
-
-  if (!myProfile || !project) {
-    return (
-      <div className="w-full flex justify-center">
-        <CircularProgress size={30} />
-      </div>
-    )
-  }
+export default function ProjectInfo({ project, setPage, refetchProject }) {
+  const mobileBreakpoint = tailwindConfig.theme.screens.sm
 
   return (
-    <div className="w-full py-4 pt-0">
-      <div>
-        {projectStudentsId.includes(myProfile.id) ? (
-          <>
-            {project[`${type}s`].map(profile => (
-              <>
-                <ProfileListItemWithIcon key={profile.id} profile={profile}>
-                  {profile.id !== myProfile.id && (
-                    <SettingsPopover
-                      profile={profile}
-                      project={project}
-                      refetchProject={refetchProject}
-                      setErrorMsg={setErrorMsg}
-                    />
-                  )}
-                </ProfileListItemWithIcon>
-              </>
-            ))}
-          </>
-        ) : (
-          <>
-            {project[`${type}s`].map(profile => (
-              <ProfileListItem key={profile.id} profile={profile} />
-            ))}
-          </>
-        )}
-        {projectMembersId.includes(myProfile.id) && (
-          <>
-            {project[`pending_invited_${type}s`].map(profile => (
-              <>
-                {projectStudentsId.includes(myProfile.id) ? (
-                  <ProfileListItemWithIcon
-                    key={profile.id}
-                    profile={profile}
-                    className="filter brightness-50"
-                  >
-                    <CloseIcon
-                      className="icon-sm color-red-hover"
-                      onClick={e =>
-                        handleCancelProjectInvitation(e, profile.user.username)
-                      }
-                    />
-                  </ProfileListItemWithIcon>
-                ) : (
-                  <ProfileListItem
-                    key={profile.id}
-                    profile={profile}
-                    className="filter brightness-50"
-                  />
-                )}
-              </>
-            ))}
-          </>
-        )}
-      </div>
-      {projectStudentsId.includes(myProfile.id) && (
-        <AddMembersModal
-          type={type}
-          project={project}
-          refetchProject={refetchProject}
-        />
+    <>
+      {visualViewport.width >
+      Number(mobileBreakpoint.slice(0, mobileBreakpoint.length - 2)) ? (
+        <MembersDesktop project={project} refetchProject={refetchProject} />
+      ) : (
+        <MembersMobile project={project} refetchProject={refetchProject} />
       )}
-      {myProfile.type === type && (
-        <>
-          {projectMembersId.includes(myProfile.id) ? (
-            <LeaveProject
-              project={project}
-              refetchProject={refetchProject}
-              setErrorMsg={setErrorMsg}
-            />
-          ) : (
-            <AskToJoinProjectModal
-              type={type}
-              project={project}
-              refetchProject={refetchProject}
-            />
-          )}
-        </>
-      )}
-      <Snackbar
-        open={errorMsg.isOpen}
-        autoHideDuration={6000}
-        onClose={() =>
-          setErrorMsg({
-            isOpen: false,
-            message: ''
-          })
-        }
-      >
-        <Alert severity="error">{errorMsg.message}</Alert>
-      </Snackbar>
-    </div>
+    </>
   )
 }
