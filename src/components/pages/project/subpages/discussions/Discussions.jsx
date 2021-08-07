9d78bf72bac8bf1460980c435748a16a271d4ea0
Felipe Carvalho <felipebarrettocarvalho@gmail.com>
2021-08-07 13:24:09 -0300

    .

--- src/components/pages/project/subpages/discussions/Discussions.jsx
+++ src/components/pages/project/subpages/discussions/Discussions.jsx
@@ -95,70 +95,77 @@ export default function Discussions({
           setErrorMsg={setErrorMsg}
         />
       )}
-      <ul>
-        {discussions.map(discussion => (
-          <li
-            key={discussion.id}
-            className="bg-transparent rounded-md shadow-lg mb-4 bg-hover cursor-pointer"
-            onClick={() => openDiscussion(discussion)}
-          >
-            <div className="px-2 pt-2 flex justify-between">
-              <div>
-                <Link href={`/user/${discussion.profile.user.username}`}>
-                  <Tooltip
-                    title={discussion.profile.user.username}
-                    className="bg-light"
-                    arrow
-                  >
-                    <div className="profile-img-sm mx-0.5">
-                      <Image
-                        src={discussion.profile.photo}
-                        layout="fill"
-                        className="cursor-pointer"
-                      />
-                    </div>
-                  </Tooltip>
-                </Link>
-              </div>
-              <div className="flex items-center">
-                <div
-                  className={`text-sm px-1 color-${discussion.category.value}`}
-                >
-                  {discussion.category.readable}
-                </div>
-                <div className="ml-2">
-                  {renderTimestamp(discussion.created_at)}
-                </div>
-                {(isProjectAdmin || myProfile.id === discussion.profile.id) && (
-                  <div
-                    className="cursor-pointer p-2"
-                    onClick={e => handleDelete(e, discussion.id)}
-                  >
-                    <DeleteIcon className="icon-sm color-red-hover" />
-                  </div>
-                )}
-              </div>
-            </div>
-            <div className="px-2 pb-2 b-bottom-transparent">
-              <strong>{discussion.title}</strong>
-            </div>
-            <div className="p-2 flex items-center">
-              <Tooltip title="Curtidas" placement="bottom" arrow>
-                <div className="mr-2">
-                  <ThumbUpAltIcon className="icon-xs" />{' '}
-                  {discussion.stars.length}
-                </div>
-              </Tooltip>
-              <Tooltip title="Respostas" placement="bottom" arrow>
+      {discussions.length ? (
+        <ul>
+          {discussions.map(discussion => (
+            <li
+              key={discussion.id}
+              className="bg-transparent rounded-md shadow-lg mb-4 bg-hover cursor-pointer"
+              onClick={() => openDiscussion(discussion)}
+            >
+              <div className="px-2 pt-2 flex justify-between">
                 <div>
-                  <CommentIcon className="icon-xs" />{' '}
-                  {discussion.replies.length}
+                  <Link href={`/user/${discussion.profile.user.username}`}>
+                    <Tooltip
+                      title={discussion.profile.user.username}
+                      className="bg-light"
+                      arrow
+                    >
+                      <div className="profile-img-sm mx-0.5">
+                        <Image
+                          src={discussion.profile.photo}
+                          layout="fill"
+                          className="cursor-pointer"
+                        />
+                      </div>
+                    </Tooltip>
+                  </Link>
                 </div>
-              </Tooltip>
-            </div>
-          </li>
-        ))}
-      </ul>
+                <div className="flex items-center">
+                  <div
+                    className={`text-sm px-1 color-${discussion.category.value}`}
+                  >
+                    {discussion.category.readable}
+                  </div>
+                  <div className="ml-2">
+                    {renderTimestamp(discussion.created_at)}
+                  </div>
+                  {(isProjectAdmin ||
+                    myProfile.id === discussion.profile.id) && (
+                    <div
+                      className="cursor-pointer p-2"
+                      onClick={e => handleDelete(e, discussion.id)}
+                    >
+                      <DeleteIcon className="icon-sm color-red-hover" />
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="px-2 pb-2 b-bottom-transparent">
+                <strong>{discussion.title}</strong>
+              </div>
+              <div className="p-2 flex items-center">
+                <Tooltip title="Curtidas" placement="bottom" arrow>
+                  <div className="mr-2">
+                    <ThumbUpAltIcon className="icon-xs" />{' '}
+                    {discussion.stars.length}
+                  </div>
+                </Tooltip>
+                <Tooltip title="Respostas" placement="bottom" arrow>
+                  <div>
+                    <CommentIcon className="icon-xs" />{' '}
+                    {discussion.replies.length}
+                  </div>
+                </Tooltip>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="w-full p-5 pt-2 text-center sm:pt-5">
+          <span>O projeto não tem discussões.</span>
+        </div>
+      )}
       <Snackbar
         open={successMsg.isOpen}
         autoHideDuration={6000}
