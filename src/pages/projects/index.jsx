bfb75b311865c66055c77d6d68142b0c339e5fbb
Carlos1043 <carlosdelia111@gmail.com>
2021-08-07 11:13:08 -0300

    notifications

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useCallback, useEffect, useState } from 'react'
 import Link from 'next/link'
 import AddIcon from '@material-ui/icons/Add'
 import CircularProgress from '@material-ui/core/CircularProgress'
@@ -8,58 +8,96 @@ import ProfileInfo from '../../components/global/profile-info/ProfileInfo'
 import ProjectsFilter from '../../components/pages/projects/ProjectsFilter'
 import useFetch from '../../hooks/useFetch'
 import { MyProfileContext } from '../../contexts/MyProfile'
+import { mutate } from 'swr'
 
 export default function Projects() {
   const { myProfile } = useContext(MyProfileContext)
+  const { data: baseProjects } = useFetch('projects/get-projects-list')
 
-  const { data: projects } = useFetch('projects/get-projects-list')
+  const [scrollCount, setScrollCount] = useState(1)
+  const [renderedProjects, setRenderedProjects] = useState([])
+  const [projectsAreFiltered, setProjectsAreFiltered] = useState(false)
 
-  const [renderedProjects, setRenderedProjects] = useState(null)
+  const handleScroll = useCallback(() => {
+    if (projectsAreFiltered) return
+
+    if (
+      Math.ceil(window.innerHeight + window.scrollY) >=
+      document.body.offsetHeight
+    ) {
+      setScrollCount(scrollCount => scrollCount + 1)
+    }
+  }, [projectsAreFiltered])
 
   useEffect(() => {
-    setRenderedProjects(projects)
-  }, [projects]) // eslint-disable-line
+    window.removeEventListener('scroll', handleScroll)
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [handleScroll])
 
-  if (!myProfile) {
+  useEffect(() => {
+    fetch(
+      `${
+        process.env.NEXT_PUBLIC_API_HOST
+      }/api/projects/get-projects-list?length=${scrollCount * 5}`
+    )
+      .then(response => response.json())
+      .then(data => {
+        mutate('projects/get-projects-list', data, false)
+        console.log(data.projects)
+        setRenderedProjects(data.projects)
+      })
+  }, [scrollCount])
+
+  useEffect(() => {
+    if (!baseProjects) return
+    setProjectsAreFiltered(
+      baseProjects.projects.length > renderedProjects.length
+    )
+  }, [baseProjects, renderedProjects])
+
+  useEffect(() => {
+    if (!baseProjects || renderedProjects.length) return
+    console.log(baseProjects.projects)
+    setRenderedProjects(baseProjects.projects)
+  }, [baseProjects]) //eslint-disable-line
+
+  if (!myProfile || !baseProjects) {
     return (
-      <Page title="Projetos | Uniconn" page="projects" loginRequired header>
-        <div>
-          <CircularProgress />
-        </div>
+      <Page title="Usuários | Uniconn" page="users" loginRequired header>
+        <CircularProgress />
       </Page>
     )
   }
 
   return (
     <Page title="Projetos | Uniconn" page="projects" loginRequired header>
-      {renderedProjects && (
-        <div className="justify-center w-full h-full flex">
-          <div className="hidden lg:w-1/3 lg:flex lg:justify-end lg:mr-10 lg:box-border">
-            <div className="w-60">
-              <div className="h-full fixed top-32">
-                <ProfileInfo profile={myProfile} />
-              </div>
-            </div>
-          </div>
-          <div className="w-full flex justify-center p-2 pt-0 lg:p-0 lg:w-2/3 lg:justify-start lg:box-border">
-            <div className="w-full" style={{ maxWidth: 600 }}>
-              <ProjectsFilter
-                projects={projects}
-                setRenderedProjects={setRenderedProjects}
-              />
-              <div className="w-full flex flex-col items-center px-2">
-                <Link href="/create-project">
-                  <div className="w-full flex items-center cursor-pointer bg-transparent bg-hover color-primary rounded-md shadow-lg p-3 mb-4">
-                    <AddIcon className="mr-2" />
-                    <strong>Criar Projeto</strong>
-                  </div>
-                </Link>
-                <ProjectList projects={renderedProjects} />
-              </div>
+      <div className="justify-center w-full h-full flex">
+        <div className="hidden lg:w-1/3 lg:flex lg:justify-end lg:mr-10 lg:box-border">
+          <div className="w-60">
+            <div className="h-full fixed top-32">
+              <ProfileInfo profile={myProfile} />
             </div>
           </div>
         </div>
-      )}
+        <div className="w-full flex justify-center p-2 pt-0 lg:p-0 lg:w-2/3 lg:justify-start lg:box-border">
+          <div className="w-full" style={{ maxWidth: 600 }}>
+            <ProjectsFilter
+              projects={baseProjects.projects}
+              setRenderedProjects={setRenderedProjects}
+            />
+            <div className="w-full flex flex-col items-center px-2">
+              <Link href="/create-project">
+                <div className="w-full flex items-center cursor-pointer bg-transparent bg-hover color-primary rounded-md shadow-lg p-3 mb-4">
+                  <AddIcon className="mr-2" />
+                  <strong>Criar Projeto</strong>
+                </div>
+              </Link>
+              <ProjectList projects={renderedProjects} />
+            </div>
+          </div>
+        </div>
+      </div>
     </Page>
   )
 }
